e12cfb8d89f5f3188eeb51ea1e8b5068027dc485
João Victor <jvsouzax@gmail.com>
2020-03-26 19:32:58 -0300

    add: incidents creation route

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,15 @@
 const express = require('express')
 
 const OngsController = require('./controllers/OngController')
-
-const connection = require('./database/connection')
+const IncidentsController = require('./controllers/IncidentController')
 
 const routes = express.Router()
 
 routes.get('/ongs', OngsController.index)
 routes.post('/ongs', OngsController.create)
+
+routes.get('/incidents', IncidentsController.index)
+routes.post('/incidents', IncidentsController.create)
+routes.delete('/incidents/:id', IncidentsController.delete)
 
 module.exports = routes
