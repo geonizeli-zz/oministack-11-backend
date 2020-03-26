66e71e35ba9063aced6eb73bee2cc9b41ddb5826
João Victor <jvsouzax@gmail.com>
2020-03-26 19:40:55 -0300

    add: profile route

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const OngsController = require('./controllers/OngController')
 const IncidentsController = require('./controllers/IncidentController')
+const ProfilesController = require('./controllers/ProfileController')
 
 const routes = express.Router()
 
@@ -11,5 +12,7 @@ routes.post('/ongs', OngsController.create)
 routes.get('/incidents', IncidentsController.index)
 routes.post('/incidents', IncidentsController.create)
 routes.delete('/incidents/:id', IncidentsController.delete)
+
+routes.get('/profile', ProfilesController.index)
 
 module.exports = routes
